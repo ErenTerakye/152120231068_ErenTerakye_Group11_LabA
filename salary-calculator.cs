4e51606606b339ee89f8e621382dcad0daa94120
ErenTerakye <erenterakye@gmail.com>
2025-05-02 17:13:18 +0300

    Add salary calculator form

---
+++ salary-calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _152120231068_ErenTerakye_Group11_LabA
+{
+    public partial class salary_calculator : Form
+    {
+        public salary_calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
